4a93d5afc09e9568247c5411f5a808b0868cbc8e
Sergio Sobrado <79481531+SergioSobrado@users.noreply.github.com>
2023-09-25 21:42:52 +0000

    BetterPractice

--- load_folder.py
+++ load_folder.py
@@ -15,23 +15,16 @@ def load_folder(path):
         if match:
             with open(path + file) as f:
                 html = f.read()
-                r.set(match.group(1), html)
-            print(match.group(0), match.group(1))
+                book_id = match.group(1)
+                search(book_id, html)
+                r.set(book_id, html)
+            print(match.group(0), book_id)
 
+def search(book_id, html):
+    soup = BeautifulSoup(html, 'html.parser')
+    tag = str(soup.p)
+    palabras = tag.split()
+    for pal in palabras:
+        r.sadd("Lea"+book_id, pal)
 
 load_folder('html/books/')
-
-def search(path):
-    files = os.listdir(path)
-    for file in files:
-        match = re.match(r'^book(\d+).html$', file)
-        if match:
-            with open(path + file, 'r') as f:
-                html = f.read()
-                soup = BeautifulSoup(html, 'html.parser')
-                tag = str(soup.p)
-                palabras = tag.split()
-                for pal in palabras:
-                    r.sadd("Lea"+match.group(1), pal)
-search('html/books/')
-
